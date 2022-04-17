8b898c8997242da0c007e9af76b1c22abca850e9
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-04-17 15:23:21 -0400

    Added: headers

---
+++ libftprintf.h
@@ -0,0 +1,21 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftprintf.h                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chughes <chughes@student.42quebec.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/04/17 15:22:14 by chughes           #+#    #+#             */
+/*   Updated: 2022/04/17 15:22:25 by chughes          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF
+# define FT_PRINTF
+
+# include <stdlib.h>
+# include <unistd.h>
+
+int	ft_printf(const char *, ...);
+
+#endif
