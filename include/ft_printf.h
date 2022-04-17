dd0b193d4eb90b5f5ecff3c31d250d1ffa0c74f9
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-04-17 16:00:10 -0400

    Added: src and include folders

--- include/ft_printf.h
+++ include/ft_printf.h
@@ -10,12 +10,13 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF
-# define FT_PRINTF
+#ifndef LIBFTPRINTF_H
+# define LIBFTPRINTF_H
 
 # include <stdlib.h>
 # include <unistd.h>
+# include <stdarg.h>
 
-int	ft_printf(const char *, ...);
+int	ft_printf(const char *format, ...);
 
 #endif
--- src/ft_printf.c
+++ src/ft_printf.c
@@ -10,4 +10,25 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "ft_printf.h"
 
+/* 
+	Needs to handle c, s, p, d, i, u, x, X, and % 
+	No buffer management
+	Bonuses: 
+		'-'	left justify
+		'0'	field is padded with 0's instead of spaces
+		'.'	precision, x.y, x is total number, y is total to the right
+		'+'	sign of number is always displayed
+		' '	blank space instead of sign
+		'#'	adds hex prefixes, like 0x
+*/
+
+int	ft_printf(const char *format, ...)
+{
+	va_list	args;
+
+	va_start(args, format);
+	va_end(args);
+	return (0);
+}
