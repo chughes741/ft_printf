7612995384b1ea98249372914ecc9e6d366363e8
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-05-01 15:04:12 -0400

    Created itox function

---
+++ ft_itox.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itohex.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: chughes <chughes@student.42quebec.com>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/05/01 09:57:54 by chughes           #+#    #+#             */
+/*   Updated: 2022/05/01 09:57:58 by chughes          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_printf.h"
+
+char	*ft_itohex(int nbr)
+{
+	int		i;
+	char	hexBase[] = "0123456789abcdef";
+	char	*rtn;
+
+	rtn = ft_calloc(9, sizeof(char));
+	if (rtn == NULL)
+		return (NULL);
+	i = -1;
+	while (++i < 8)
+		rtn[i] = hexBase[((nbr >> (28 - (i * 4))) & 0xF)];
+	return (rtn);
+}
