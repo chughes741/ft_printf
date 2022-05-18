db62a0767479c9360b4a667920730a9b0e301e8f
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-05-18 14:25:18 -0400

    Reformatted ft_print_arg for norm

--- ft_print_arg.c
+++ ft_print_arg.c
@@ -12,87 +12,23 @@
 
 #include "ft_printf.h"
 
-static void	ft_space_to_zero(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i] && str[i] == ' ')
-	{
-		str[i] = '0';
-		i++;
-	}
-	return ;
-}
-
-static char	*ft_set_width(char *str, int width)
-{
-	if (ft_strlen(str) >= (unsigned long)width)
-		return (str);
-	width -= ft_strlen(str);
-	while ((width--) > 0)
-		str = ft_str_append(" ", str);
-	return (str);
-}
-
-static void	ft_left_justify(char *str)
-{
-	int	i;
-	int	j;
-
-	if (str[0] != ' ')
-		return ;
-	i = 0;
-	j = 0;
-	while (str[j] == ' ')
-		j++;
-	while (str[j])
-	{
-		str[i] = str[j];
-		str[j] = ' ';
-		i++;
-		j++;
-	}
-	return ;
-}
-
-static void	ft_strupper(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i])
-	{
-		if (str[i] >= 'a' && str[i] <= 'z')
-			str[i] ^= 1 << 5;
-		i++;
-	}
-}
-
 static int	ft_putchar(int c)
 {
 	return (write(1, &c, 1));
 }
 
-static char	*ft_format(t_mod *mod, char *str)
+static char	*ft_get_num(t_mod *mod, va_list args, char *output)
 {
-	if (mod->width >= 0 && mod->width >= mod->precision)
-		str = ft_set_width(str, mod->width);
-	else if (mod->precision >= 0 && mod->precision > mod->width)
-		str = ft_set_width(str, mod->precision);
-	if (mod->hash || mod->specifier == 'p')
-		str = ft_str_append("0x", str);
-	if (mod->specifier == 'X')
-		ft_strupper(str);
-	if (mod->plus)
-		str = ft_str_append("+", str);
-	if (mod->dash)
-		ft_left_justify(str);
-	if (mod->space && str[0] != ' ')
-		str = ft_str_append(" ", str);
-	if (mod->zero)
-		ft_space_to_zero(str);
-	return (str);
+	free(output);
+	if (mod->specifier == 'd' || mod->specifier == 'i')
+		output = ft_itoa(va_arg(args, int));
+	if (mod->specifier == 'u')
+		output = ft_utoa(va_arg(args, unsigned int));
+	if (mod->specifier == 'x' || mod->specifier == 'X')
+		output = ft_itox(va_arg(args, unsigned int));
+	if (mod->specifier == 'p')
+		output = ft_ptoa((va_arg(args, uintptr_t)));
+	return (output);
 }
 
 void	ft_print_arg(t_mod *mod, va_list args, int *count)
@@ -106,26 +42,9 @@ void	ft_print_arg(t_mod *mod, va_list args, int *count)
 		*count += ft_putchar(va_arg(args, int));
 	if (mod->specifier == 's')
 		output = ft_str_append(va_arg(args, char *), output);
-	if (mod->specifier == 'd' || mod->specifier == 'i')
-	{
-		free(output);
-		output = ft_itoa(va_arg(args, int));
-	}
-	if (mod->specifier == 'u')
-	{
-		free(output);
-		output = ft_utoa(va_arg(args, unsigned int));
-	}
-	if (mod->specifier == 'x' || mod->specifier == 'X')
-	{
-		free(output);
-		output = ft_itox(va_arg(args, unsigned int));
-	}
-	if (mod->specifier == 'p')
-	{
-		free(output);
-		output = ft_ptoa((va_arg(args, uintptr_t)));
-	}
+	if (mod->specifier == 'd' || mod->specifier == 'i' || mod->specifier == 'u'\
+	|| mod->specifier == 'x' || mod->specifier == 'X' || mod->specifier == 'p')
+		output = ft_get_num(mod, args, output);
 	if (output == NULL)
 		output = ft_strdup("(null)");
 	output = ft_format(mod, output);
