7c003c0c73ec08a0419eb7d6fa7e1edf639e086e
Cole Hughes - 42 <coltenhughes@gmail.com>
2022-05-17 20:06:43 -0400

    Shortened str_append

--- ft_str_append.c
+++ ft_str_append.c
@@ -23,22 +23,15 @@ char	*ft_str_append(char *s1, char *s2)
 		free(s2);
 		return (NULL);
 	}
-	i = 0;
-	j = 0;
-	rtn = malloc((ft_strlen(s1) + ft_strlen(s2) + 1) * sizeof(char));
+	i = -1;
+	j = -1;
+	rtn = ft_calloc((ft_strlen(s1) + ft_strlen(s2) + 1), sizeof(char));
 	if (rtn == NULL)
 		return (NULL);
-	while (s1[i])
-	{
+	while (s1[++i])
 		rtn[i] = s1[i];
-		i++;
-	}
-	while (s2[j])
-	{
+	while (s2[++j])
 		rtn[i + j] = s2[j];
-		j++;
-	}
-	rtn[i + j] = '\0';
 	free(s2);
 	return (rtn);
 }
